18fe11534906fce90d92c7da815666d013d346b2
VictorEriksonAlvarezNavarro <zetsuengt@gmail.com>
2018-07-02 22:26:09 -0700

    disenho de la interfaz de la app

--- app/src/main/java/com/example/erikson/calculadorauno/MainActivity.java
+++ app/src/main/java/com/example/erikson/calculadorauno/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.erikson.calculadorauno;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
